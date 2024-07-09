8f2f7e714528a15717c405fa9e161a0e93de7fab
Derto42 <125327962+derto42@users.noreply.github.com>
2024-07-09 16:52:05 -0500

    Restructure 2.0 / Netlify Update
    Static files moved into public
    
    Dynamic files to src
    
    Netlify is working (https://r2p2.netlify.app/)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify your build output directory
+    rollupOptions: {
+      input: {
+        index: 'index.html',
+        About: 'About.html',
+        Services: 'Services.html',
+        Testimonial: 'Testimonial.html',
+        Schedule: 'Schedule.html',
+        Learn: 'Learn.html',
+        Shop: 'Shop.html',
+
+        Healing: 'Healing.html',
+        Movement: 'Movement.html',
+        Nutrition: 'Nutrition.html',
+        Yoga: 'Yoga.html',
+        // Add more entries as needed for each HTML file
+      }
+    }
+  }
+});
